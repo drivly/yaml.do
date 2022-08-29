3216cf04a63811199c19c7c16532cd98d9c0086d
Nathan Clevenger <nateclev@gmail.com>
2022-08-29 08:14:30 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,13 @@ import yaml from 'yaml'
 export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
-    const data = await fetch('https:/' + pathname + search, req).then(res => res.text())
-    return new Response(JSON.stringify(yaml.parse(data), null, 2), { headers: { 'content-type': 'application/json' }})
+    const res = await fetch('https:/' + pathname + search, req).then(res => res.text())
+    if (res.headers.get('content-type').includes('json)) {
+      const data = await res.json()      
+      return new Response(yaml.stringify(data), null, 2), { headers: { 'content-type': 'application/yaml' }})
+    } else {
+      const data = await res.text()
+      return new Response(JSON.stringify(yaml.parse(data), { headers: { 'content-type': 'application/json' }})
+    }
   }
 }
