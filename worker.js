a7ca9b64e6ef3e2f16d33e645cb3c61b1a39a088
Nathan Clevenger <nateclev@gmail.com>
2022-08-29 08:26:14 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import yaml from 'yaml'
 export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
-    const res = await fetch('https:/' + pathname + search, req).then(res => res.text())
+    const res = await fetch('https:/' + pathname + search, req)
     if (res.headers.get('content-type').includes('json')) {
       const data = await res.json()      
       return new Response(yaml.stringify(data), { headers: { 'content-type': 'application/yaml' }})
