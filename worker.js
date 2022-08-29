6547b503d6d3dd860a7dd4a33051b68a55c40d10
Nathan Clevenger <nateclev@gmail.com>
2022-08-29 08:16:49 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
     const res = await fetch('https:/' + pathname + search, req).then(res => res.text())
-    if (res.headers.get('content-type').includes('json)) {
+    if (res.headers.get('content-type').includes('json')) {
       const data = await res.json()      
       return new Response(yaml.stringify(data), null, 2), { headers: { 'content-type': 'application/yaml' }})
     } else {
